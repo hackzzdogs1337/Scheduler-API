b0672d80592ab2e5d9555b91a4018672f5edd529
hackzzdogs1337 <anirudh.narayanan00@gmail.com>
2020-06-27 17:04:26 +0530

    Commited successfull

---
+++ api_project/api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import time,requests
+from rest_framework.response import Response
+from rest_framework.views import APIView
+# Create your views here.
+class PingView(APIView):
+    def get(self,request):
+        return Response({'status':'OK'})
+
+class ScheduleView(APIView):
+    def get(self,request):
+        url=request.query_params['url']
+        url_datetime=request.query_params['time']
+        if(time.strftime('%d-%m-%Y %H:%M')==url_datetime):
+            response=requests.get(url,verify=False)
+            return Response({'status_code':response.status_code})
+        return Response({'respone':"The current time doesnt match"})
